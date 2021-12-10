e30ef463c19b966d98316ed041b0aa0777ff0e94
Erik Vesteraas <erik@vestera.as>
2021-12-10 18:56:52 +0100

    Now does what it needs to: Make a gradient

---
+++ README.md
@@ -0,0 +1,4 @@
+# gradient
+
+Because sometimes I just need a specific gradient.
+Initially, one that passes through the hue spectrum.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use hsl::HSL;
+use image::{Rgb, RgbImage};
+
+fn main() -> Result<(), impl Error> {
+    let width = 3508;
+    let height = 3508;
+    let mut buffer = RgbImage::new(width, height);
+    for x in 0..width {
+        for y in 0..height {
+            let hsl = HSL {
+                h: (((x + y) as f64) / ((width + height) as f64)) * 360.0,
+                s: 1.0,
+                l: 0.5
+            };
+            let rgb = hsl.to_rgb();
+            buffer.put_pixel(x, y, Rgb([rgb.0, rgb.1, rgb.2]));
+        }
+    }
+    buffer.save("out.png")
 }
